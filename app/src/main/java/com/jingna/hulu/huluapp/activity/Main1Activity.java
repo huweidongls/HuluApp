a0d31f37165e85ac766ed57fb8d51c978a05255a
QwangShuai <31304278+QwangShuai@users.noreply.github.com>
2018-09-15 20:18:09 +0800

    123

--- app/src/main/java/com/jingna/hulu/huluapp/activity/Main1Activity.java
+++ app/src/main/java/com/jingna/hulu/huluapp/activity/Main1Activity.java
@@ -40,9 +40,9 @@ public class Main1Activity extends BaseActivity {
     private void initBanner() {
 
         List<String> images = new ArrayList<>();
-        images.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536678658773&di=8fa8eb91cc5db5c39f4e01b94114e2e4&imgtype=0&src=http%3A%2F%2Fwww.wallcoo.com%2Fnature%2FDigital_3D_HD_Wallpaper_1920x1200%2Fwallpapers%2F1920x1200%2Fdigital_3D_wallpaper_citidel2k81.jpg");
-        images.add("https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=418259163,3789341045&fm=26&gp=0.jpg");
-        images.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536678658772&di=40a22fe55d78a265f57c29b77b8f0327&imgtype=0&src=http%3A%2F%2Fimage.17173.com%2Fbbs%2Fv1%2F2010%2F03%2F24%2F1269416139181.jpg");
+        images.add("http://47.92.127.1:8088/upload/jn_1.jpg");
+        images.add("http://47.92.127.1:8088/upload/jn_2.jpg");
+        images.add("http://47.92.127.1:8088/upload/jn_3.jpg");
         //设置banner样式
         banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
         //设置图片加载器
